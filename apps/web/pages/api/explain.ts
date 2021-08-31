937ae6a7d7ae0ab4acf8314f7d2a028fe24fc57c
Brian Li <zhenbangli@gmail.com>
2021-08-31 12:54:48 -0500

    feat(api): add api route to get code explanation

---
+++ apps/web/pages/api/explain.ts
@@ -0,0 +1,56 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { queryCodex } from "@codexplain/common";
+import Cors from "cors";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type ApiError = {
+  message: string;
+};
+
+type Data =
+  | {
+      output: string;
+    }
+  | ApiError;
+
+const cors = Cors({
+  methods: ["GET", "HEAD"],
+  origin: "*",
+});
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(
+  req: NextApiRequest,
+  res: NextApiResponse,
+  fn: Function
+) {
+  // print request origin
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result: any) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+
+      return resolve(result);
+    });
+  });
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  await runMiddleware(req, res, cors);
+  try {
+    const response = await queryCodex({
+      prompt: req.body.prompt,
+      apiKey: process.env.OPENAI_API_KEY!,
+      stop: req.body.isBlock ? ["##", "```"] : ["##"],
+    });
+    const output = response.data.choices[0].text;
+    res.status(200).json({ output });
+  } catch (e) {
+    res.status(500).json({ message: e.message });
+  }
+}
